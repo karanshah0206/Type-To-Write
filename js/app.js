a40d4a1e82829218fc407174c7017b2b7247ae63
Karan Shah <karanshah0206@outlook.com>
2020-05-23 23:10:01 +0530

    Refined Info Experience

--- js/app.js
+++ js/app.js
@@ -358,9 +358,10 @@ function infotab () {
   document.getElementById('info-tab').classList.toggle('info-hider');
   if(document.getElementById('info-tab').classList.contains('info-hider') == true) {
     document.getElementById('info-image').src = 'img/information-button.png';
+    document.getElementById('top-most').classList.remove('info-hider');
   }
   else {
     document.getElementById('info-image').src = 'img/cross-button.png';
-    window.scrollTo(0, 0);
+    document.getElementById('top-most').classList.add('info-hider');
   }
 }
